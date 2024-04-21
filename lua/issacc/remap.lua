fae5d9af03b19160f426d5a21e4d74371e197312
wavy_ramirez <620ir08@gmail.com>
2024-04-21 03:19:52 -0700

    Adding buffer delete keybind <leader>d

--- lua/issacc/remap.lua
+++ lua/issacc/remap.lua
@@ -4,3 +4,5 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 vim.keymap.set("n", "[b", vim.cmd.bprevious)
 vim.keymap.set("n", "]b", vim.cmd.bnext)
+
+vim.keymap.set("n", "<leader>d", vim.cmd.bd)
