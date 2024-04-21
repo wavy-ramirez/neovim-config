f0565b859f9e30e8f7848cf0969d9774465c2faf
wavy-ramirez <620ir08@gmail.com>
2024-04-21 01:33:18 -0700

    init

---
+++ lua/issacc/init.lua
@@ -0,0 +1,4 @@
+require("issacc.remap")
+require("issacc.options")
+require("issacc.lazy")
+require("issacc.bufferline")
---
+++ lua/issacc/options.lua
@@ -0,0 +1,9 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.scrolloff = 25
+
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+
+vim.opt.wrap = false
