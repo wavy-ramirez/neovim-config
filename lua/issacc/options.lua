b86f30cfb61c8cc30d3f12e11e11f1b2e5ee6c54
wavy_ramirez <620ir08@gmail.com>
2024-04-24 18:18:53 -0700

    Some Random Changes

--- lua/issacc/options.lua
+++ lua/issacc/options.lua
@@ -5,5 +5,9 @@ vim.opt.scrolloff = 25
 
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.expandtab = true
+vim.opt.autoindent = true
+vim.g.rust_recommended_style = false
 
 vim.opt.wrap = false
